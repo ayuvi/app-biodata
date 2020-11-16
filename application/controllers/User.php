007bdb54e7888a687d317cfc222ab22fcb1fab4d
ayuvi <muhammadayuvi@gmail.com>
2020-11-16 07:58:28 +0700

    edit profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -24,5 +24,29 @@ class User extends CI_Controller
 		$this->load->view('templates/footer');
 	}
 
-	
+	public function edit()
+	{
+		$data['title'] = 'Edit Profile';
+		// ambil data dari user yang ada dari session
+		// select email.session from user where email = email.session
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+		$this->form_validation->set_rules('name', 'Full Name', 'required|trim');
+
+		if($this->form_validation->run() == false) {
+			$this->load->view('templates/header', $data);
+			$this->load->view('templates/sidebar', $data);
+			$this->load->view('templates/topbar', $data);
+			$this->load->view('user/edit', $data);
+			$this->load->view('templates/footer');
+
+		} else {
+			$name = $this->input->post('name');
+			$email = $this->input->post('email');
+
+			$this->set('name', $name);
+			$this->db->where('email', $email);
+			$this->db->update('user');
+		}
+	}
 }
